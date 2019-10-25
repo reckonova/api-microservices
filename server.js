003acf49e25a0d44d887a20e19d16f6ee5f8409e
Glitch (1fcc-timestamp-dcc) <none>
2019-10-25 13:00:52 +0000

    ✊🛳 Updated with Glitch

--- server.js
+++ server.js
@@ -9,29 +9,43 @@ app.get("/", function (req, res,next) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/api/timestamp/', function (req, res, next) { 
+  let date_string = new Date(Date.now());
+  let date_time = date_string.toUTCString(); 
+  var timestamp = { "unix": date_string.getTime(),"utc": date_string.toUTCString()}
+  res.json(timestamp);
+})
 
 
-app.route('/api/timestamp/:date_string').get( function (req, res, next) { 
-  let date_string = req.params.date_string;
- 
-   if(!isNaN(date_string)) {
-     date_string = parseInt(date_string) * 1000; 
-   }
-  
+app.route('/api/timestamp/:date_string?').get( function (req, res, next) { 
+   var date_string = null;
+    if (req.params.date_string !== undefined) {
+      var unixTimestamp = parseInt(req.params.date_string*1);
+      if (isNaN(unixTimestamp)) {
+        date_string = new Date(req.params.date_string);
+      } else {
+        date_string = new Date(unixTimestamp);
+      }
+      
+    } 
   let numberDate = new Date(date_string).getTime(); 
   let letterDate = new Date(date_string).toUTCString(); 
   
+   var response = date_string == "Invalid Date" ? 
+      { error: "Invalid Date" } :
+      { "unix": numberDate,
+        "utc": letterDate
+      };
+    
+    res.json(response);
   res.json({numberDate: numberDate, letterDate: letterDate});
 })
 
 
-app.get('/api/timestamp/', function (req, res, next) { 
-  let date_string = new Date(); 
-  let date_time = date_string.toUTCString(); 
-  res.json({"unix": date_string, "utc": date_time});
-})
 
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+listener;
